558153a3719f622bce9cf91665d427e7eb24d59c
luka-kavkler <luka.kavkler@gmail.com>
2026-06-05 19:07:50 +0200

    predetermined system, extended powerMethod

--- Methods.py
+++ Methods.py
@@ -3,11 +3,12 @@ import scipy
 import pandas as pd
 import math
 from warnings import warn
+import itertools
 
 TOL = 1.48e-08;
 MAX_STEPS = 50;
 
-def GaussNewton(F, x, maxSteps = MAX_STEPS, tol=TOL):
+def gaussNewton(F, x, maxSteps = MAX_STEPS, tol=TOL):
     """Takes in vector function F, and starting aproximation of x;
     returns back an aproximation for a zero of F function
     """
@@ -30,14 +31,14 @@ def GaussNewton(F, x, maxSteps = MAX_STEPS, tol=TOL):
     return x;
 
 
-def HotelingReduction(A, lamb, x):
-        """Returns an adjusted array for further eigen value analysis"""
-        return A - lamb * np.outer(x,x);
+def hotelingReduction(A, lamb, x):
+    """Returns an adjusted array for further eigen value analysis (meant for usage with powerMethod)"""
+    return A - lamb * np.outer(x,x);
 
 
-def RaylK(A,x):
-        """Returns raylleighs kvocient? for a normalised vector x"""
-        return np.transpose(x) @ A @ x;
+def raylK(A,x):
+    """Returns raylleighs kvocient? for a normalised vector x"""
+    return np.transpose(x) @ A @ x;
 
 
 def powerMethod(A, x, max_steps = MAX_STEPS, tol = TOL):
@@ -48,12 +49,25 @@ def powerMethod(A, x, max_steps = MAX_STEPS, tol = TOL):
     for _ in range(max_steps):
         x = A @ x;
         x = x/np.linalg.norm(x);
-        lamb = RaylK(A,x);
+        lamb = raylK(A,x);
         if np.linalg.norm(A @ x - lamb * x) < tol:
              return (lamb, x)
         
     warn("Max steps reached")
     return (lamb, x)
+
+def extendedPowerMethod(A, x, n, max_steps = MAX_STEPS, tol = TOL):
+    """Returns first n dominant pairs organised by size of lambda descending (lambda, v)"""
+    pairs = list();
+    B = A.copy();
+    v = x;
+    for _ in range(n):
+        eigenPair = powerMethod(B, v, max_steps=max_steps, tol=tol);
+        pairs.append(eigenPair);
+        B = hotelingReduction(B, eigenPair[0], eigenPair[1]);
+    return pairs
+
+
 
 def inversePowerMethod(A, x, sigma, max_steps = MAX_STEPS, tol = TOL):
     """Takes: square matrix A, aprox of dominant eigen vector x, n number of lambdas
@@ -74,7 +88,7 @@ def inversePowerMethod(A, x, sigma, max_steps = MAX_STEPS, tol = TOL):
         
         x = x/np.linalg.norm(x);
 
-        lamb = RaylK(A_start,x);
+        lamb = raylK(A_start,x);
         
         if np.linalg.norm(A_start @ x - lamb * x) < tol:
              return (lamb, x)
@@ -89,14 +103,48 @@ def singularMatrixAproximation(A, k):
     A_ = np.zeros(np.shape(A));
 
     A_ = U[:, :k] @ np.diag(s[:k]) @ Vh[:k, :];
-    
+
     return A_
 
-A = np.array([[1, 0], [4, 8]]);
-x = np.transpose(np.array([1, -2]));
-print(inversePowerMethod(A, x, 2))
-print(singularMatrixAproximation(A, 1))
 
+def predeterminedSystemFunction(X, Y, F):
+    """Finds a function that is a linear combination of functions in list of functions F,
+    that aproximates the y values best by MLS and a vector of koeficients that represent the actual solution of the predetermined system"""
+    X : list[int];
+    Y : list[int];
+    F : list[function];
+
+    n = len(X);
+    m = len(F);
+
+    A = np.array(n*[m*[0]], dtype=float);
+    for i in range(n):
+        A[i, :] = np.array([x[0](x[1]) for x in itertools.product(F, [X[i]])]);
+
+    Q, R = np.linalg.qr(A);
+    koef = np.linalg.solve(R, np.transpose(Q) @ Y)
+
+    def f(x):
+        """Aproximating function for the overdeterminated system"""
+        y = 0;
+        for i in range(len(koef)):
+            y += koef[i] * F[i](x);
+        return y;
+
+    return f, koef;
+
+def predeterminedSystem(A,b):
+    """Solves predetermined system and returns best solution vector by MLK"""
+    Q, R = np.linalg.qr(A);
+    z = np.linalg.solve(R, np.transpose(Q) @ b);
+    return z;
+
+
+X = [0.21, 0.66, 0.93, 1.25, 1.75, 2.03, 2.24, 2.57, 2.87, 2.98];
+Y = [0.25, -0.27, -1.12, -0.45, 0.28, 0.13, -0.27, 0.26, 0.58, 1.03];
+F = [math.exp, math.log, math.sin, math.cos];
+
+print(predeterminedSystem(X,Y,F))
 
         
 
